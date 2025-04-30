acbdf1ecd520eaf8b5f25789c16f738f62b4b8db
Paola Novick <paola.novick@davinci.edu.ar>
2025-04-30 09:31:41 -0300

    estilos

--- src/servicios/especificos/datosAgenciaSimulada.ts
+++ src/servicios/especificos/datosAgenciaSimulada.ts
@@ -1,56 +1,53 @@
-// src/servicios/datosAgenciaSimulada.ts
-import { AgenciaBackData } from "./transformarAgenciaBackData";
-
 export const datosAgenciaSimuladaBack: AgenciaBackData = {
   idAgencia: 3,
   nombre: "Inntravel Viajes",
-  logo: "/logosf.png",
+  logo: "/logosf2.png",
   tipografia_agencia: "Verdana",
-  color_tipografia_agencia: "#2E003E",
-  color_fondo_app: "#b8add5", // Lila muy suave
+  color_tipografia_agencia: "#014D66",
+  color_fondo_app: "#D0F0F8", // Celeste muy suave
 
-  color_principal: "#7B1FA2", // Violeta fuerte
-  color_secundario: "#e1edd7", // Lila claro
-  color_terciario: "#4A0072", // Violeta oscuro
+  color_principal: "#0288D1", // Celeste fuerte
+  color_secundario: "#B3E5FC", // Celeste claro
+  color_terciario: "#01579B", // Celeste oscuro
 
   header_imagen_background: "/fondo-playa.jpg",
   header_imagen_background_opacidad: 0.2,
-  header_video_background: "/video.mp4",
+  header_video_background: "/nieve.mp4",
   header_video_background_opacidad: 0.3,
 
   buscador_tipografia: "Verdana",
   buscador_tipografia_color: "#FFFFFF",
-  buscador_tipografia_color_label: "#7B1FA2",
-  buscador_inputColor: "#2E003E",
+  buscador_tipografia_color_label: "#0288D1",
+  buscador_inputColor: "#014D66",
   buscador_inputFondoColor: "#FFFFFF",
-  buscador_color_primario: "#7B1FA2",
-  buscador_color_secundario: "#4A0072",
+  buscador_color_primario: "#0288D1",
+  buscador_color_secundario: "#01579B",
   buscador_color_terciario: "white",
 
   publicidad_existe: true,
   publicidad_titulo: "imperdibles",
-  publicidad_tipografia_color: "#4a0387",
-  publicidad_color_primario: "#7B1FA2",
-  publicidad_color_secundario: "#CE93D8",
-  publicidad_color_terciario: "#4A0072",
+  publicidad_tipografia_color: "#0277BD",
+  publicidad_color_primario: "#0288D1",
+  publicidad_color_secundario: "#81D4FA",
+  publicidad_color_terciario: "#01579B",
   publicidad_imagen_1: "paginaWeb/banner1.png",
-  publicidad_imagen_2: "paginaWeb/bannerC.png",
-  publicidad_imagen_3: "paginaWeb/bannerB.png",
+  publicidad_imagen_2: "paginaWeb/banner2.png",
+  publicidad_imagen_3: "paginaWeb/banner3.png",
 
   tarjetas_titulo: "Destinos recomendados",
   tarjetas_tipografia: "Verdana",
   tarjetas_tipografia_color: "white",
-  tarjetas_tipografia_color_titulo: "#4a0387",
-  tarjetas_tipografia_color_contenido: "#4A0072",
-  tarjetas_color_primario: "#4a0387",
+  tarjetas_tipografia_color_titulo: "#0277BD",
+  tarjetas_tipografia_color_contenido: "#01579B",
+  tarjetas_color_primario: "#0277BD",
   tarjetas_color_secundario: "white",
-  tarjetas_color_terciario: "#4A0072",
+  tarjetas_color_terciario: "#01579B",
 
   banner_registro_titulo: "Registrate y viajá con nosotros",
   banner_registro_tipografia_color: "#FFFFFF",
-  banner_registro_color_primario: "#4a0387",
-  banner_registro_color_secundario: "#CE93D8",
-  banner_registro_color_terciario: "#4A0072",
+  banner_registro_color_primario: "#0277BD",
+  banner_registro_color_secundario: "#81D4FA",
+  banner_registro_color_terciario: "#01579B",
 
   quienes_somos_es: "En Inntravel Viajes, somos una agencia dedicada a ofrecer experiencias excepcionales y memorables, adaptadas a las necesidades de cada viajero. Nos apasiona ayudar a descubrir el mundo de manera segura, emocionante y única, con un enfoque completamente personalizado para cada cliente. Creemos en la importancia de combinar la calidez humana con las últimas innovaciones tecnológicas, lo que nos permite mantenernos a la vanguardia del sector y ofrecerte siempre las mejores opciones disponibles. Esta sinergia entre lo personal y lo digital es lo que nos permite garantizarte un servicio de calidad, eficiente y adaptado a tus expectativas. En Inntravel Viajes, no solo organizamos viajes, sino que creamos recuerdos inolvidables, brindándote la tranquilidad y el confort que necesitas para disfrutar de cada momento. Todo nuestro esfuerzo está dirigido a brindarte una experiencia de viaje única, que combine la excelencia en el servicio con lo último en tecnología.",
   quienes_somos_en: "Inntravel Viajes was born with the aim of offering travelers of all ages the tools and advice to explore the world in their own way.",
@@ -68,7 +65,7 @@ export const datosAgenciaSimuladaBack: AgenciaBackData = {
   footer_direccion: "xxxxxxxxxxx 12345 ",
   footer_ciudad: "abbcdef, Buenos Aires",
   footer_pais: "Argentina",
-  footer_color_primario: "4a0387",
-  footer_color_secundario: "#4a0387",
-  footer_color_terciario: "#b8add5"
+  footer_color_primario: "#0277BD",
+  footer_color_secundario: "#0277BD",
+  footer_color_terciario: "#D0F0F8"
 };
